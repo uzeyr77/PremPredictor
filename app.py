a9889538037adc185394efddd645d0d20d9545a4
Uzeyr_Salah <uzeyrsheikhabdirahman@gmail.com>
2026-03-08 03:32:40 -0400

    auto update for prem table file created

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
-    return "homepage"
+    return render_template('index.html')
 
 @app.route("/title")
 def leagueTitle():
